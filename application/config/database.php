c94a055ef89e52d1a9549444988ea91cc371a21c
Adam Griffiths <adamgriffiths@zenbe.com>
2009-08-03 23:56:35 +0100

    Removed databse settings from database config file :)

--- application/config/database.php
+++ application/config/database.php
@@ -38,9 +38,9 @@ $active_group = "default";
 $active_record = TRUE;
 
 $db['default']['hostname'] = "localhost";
-$db['default']['username'] = "adam";
-$db['default']['password'] = "09022687";
-$db['default']['database'] = "ci_cms";
+$db['default']['username'] = "";
+$db['default']['password'] = "";
+$db['default']['database'] = "";
 $db['default']['dbdriver'] = "mysql";
 $db['default']['dbprefix'] = "";
 $db['default']['pconnect'] = TRUE;
